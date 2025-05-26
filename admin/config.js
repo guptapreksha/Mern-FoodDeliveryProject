522bd9154f40d7dc1ed6930bd77828a152c92a92
Preksha Gupta <106445265+guptapreksha@users.noreply.github.com>
2025-05-27 02:08:55 +0530

    Update config.js

--- admin/config.js
+++ admin/config.js
@@ -1,3 +1,3 @@
 // src/config.js
-export const url = "http://localhost:4001"; // or your deployed URL
-// export const url = "https://mern-project-backend-y6i5.onrender.com";
+// export const url = "http://localhost:4001"; // or your deployed URL
+export const url = "https://mern-fooddeliveryproject-backend.onrender.com";
