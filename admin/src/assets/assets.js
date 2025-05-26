9d6e6aac573c3acfb98bd1d6b3ffee9afb70d0c4
Preksha Gupta <106445265+guptapreksha@users.noreply.github.com>
2025-05-27 02:08:01 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,6 +5,9 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
+export const url = 'https://mern-fooddeliveryproject-backend.onrender.com'
+
+
 export const assets ={
     logo,
     add_icon,
